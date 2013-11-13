9b9ad8156f024a45ccd710309dabf155e160f237
Thibaud Arguillere <thibarg.web@free.fr>
2013-11-13 09:25:50 -0500

    First commit

---
+++ src/main/resources/web/nuxeo.war/scripts/a-framework.js
@@ -0,0 +1,15 @@
+/* a-framework.js */
+
+function ZeFrameworkInit(inSomething) {
+	if(typeof inSomething == "string") {
+		alert("Initialisation. Received: < " + inSomething + ">");
+	} else {
+		alert("Initialisation. inSomething is not a string, it is a " + typeof inSomething);
+	}
+}
+
+function ZeFrameworkSomething(inStr) {
+	alert(inStr);
+	// id of div Hard-coded for the test
+	jQuery("#ze_dashboard").text("received: " + inStr);
+}
---
+++ src/main/resources/web/nuxeo.war/scripts/test-js-widget.js
@@ -0,0 +1,6 @@
+/* test-js-widget.js */
+
+function doSomething() {
+	// Calling the framework
+	ZeFrameworkSomething("Alors ?");
+}
